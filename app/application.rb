3599bbf295b112e1ed2d75485a18911c160ec260
Blaine Richardson <blainerrichardson@gmail.com>
2017-10-03 00:29:35 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,6 +1,7 @@
 class Application
 
   def call(env)
+    resp = Rack::Response.new
     now = Time.now
     if now.hour >=12
       resp.write "Good Afternoon"
@@ -9,4 +10,5 @@ class Application
     end
     resp.finish
   end
+
 end
